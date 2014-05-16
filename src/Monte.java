54f2f02197a4cbf60168ce90f2e4ef5b81907cca
KosherBacon <j.harris.kahn@gmail.com>
2014-05-16 11:10:26 -0700

    Started thread pooling.

--- src/Monte.java
+++ src/Monte.java
@@ -1,5 +1,9 @@
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 
 public class Monte {
@@ -9,6 +13,30 @@ public class Monte {
 		BigDecimal t = BigDecimal.ZERO, i = BigDecimal.ZERO, g = new BigDecimal("10000000"); // Number of points
 		
 		RadiusService radius = (x, y) -> {return Math.sqrt(x*x+y*y);};		
+		
+		int cpus = Runtime.getRuntime().availableProcessors();
+		int maxThreads = cpus * 1;
+		maxThreads = (maxThreads > 0 ? maxThreads : 1);
+		
+		boolean running = true;
+		
+		ExecutorService executorService =
+				new ThreadPoolExecutor(
+					maxThreads,
+					maxThreads,
+					1,
+					TimeUnit.MINUTES, 
+					new ArrayBlockingQueue<Runnable>(maxThreads, true),
+					new ThreadPoolExecutor.CallerRunsPolicy());
+		
+		Runnable calc = new Runnable() {
+			@Override
+			public void run() {
+				while (running) {
+					
+				}
+			}
+		};
 		
 		while (t.compareTo(g) < 0) {
 			double r = radius.getRadius(Math.random(), Math.random());
